f13e1dbbec2b7f4c9981e2188160cd17c4bfa6e4
chenliang <chenl@shukun.net>
2020-11-13 18:06:45 +0800

    change docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,5 @@
-if [ "`ls -l /var/lib/mysql|wc -l`" > "3" ]; then
-  mv /var/lib/mysql_bak/* /var/lib/mysql/
-else
-  echo "mysql file already exists"
-fi
-
-if [ "`ls -A /root/`" = "" ]; then
-  mv /root_bak/* /root/
-else
-  echo "root file already exists"
-fi
-
-if [ "`ls -A /root/`" = "" ]; then
-  mv /root_bak/* /root/
-else
-  echo "root file already exists"
-fi
-
+mv /var/lib/mysql_bak/* /var/lib/mysql/
+mv /root_bak/* /root/
 mv /home/neople_bak/* /home/neople/
 cd /root
 ./run
