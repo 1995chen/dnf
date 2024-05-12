0564ff3208dbf34b894784abd418ed602b28134a
LiangChen <chenl2448365088@gmail.com>
2024-05-12 08:34:36 +0800

    chore: update netbird script

--- build/dnf_data/netbird/start_netbird.sh
+++ build/dnf_data/netbird/start_netbird.sh
@@ -14,7 +14,7 @@ if [ -z "$PUBLIC_IP" ] && [ -n "$NB_SETUP_KEY" ] && [ -n "$NB_MANAGEMENT_URL" ];
   echo "starting netbird service[$NB_MANAGEMENT_URL] use setup_key: $NB_SETUP_KEY"
   service netbird start
   # 保持前台运行
-  netbird up --foreground-mode --config=/data/netbird/config.json --log-level=info --log-file=/data/log/netbird/netbird.log
+  netbird up --foreground-mode --config /data/netbird/config.json --log-level info --log-file /data/log/netbird/netbird.log
 else
     echo "no need to start netbird"
     sleep 5
