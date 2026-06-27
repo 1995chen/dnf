59622a5ec08dc7542b6a02426620965e1047791b
1995chen <chenl2448365088@gmail.com>
2026-06-27 10:22:21 +0800

    fix: tailscale reset connection

--- build/dnf_data/home/template/init/tailscale/start_tailscaled.sh
+++ build/dnf_data/home/template/init/tailscale/start_tailscaled.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
+mkdir -p /dev/net
+if [ ! -c /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+    chmod 600 /dev/net/tun
+fi
 # 检查PUBLIC IP
 if [ -z "$PUBLIC_IP" ] && [ -n "$TS_AUTH_KEY" ] && [ -n "$TS_LOGIN_SERVER" ]; then
-    /usr/bin/tailscaled --state=/data/tailscale/tailscaled.state --socket=/data/tailscale/tailscaled.sock
+    /usr/bin/tailscaled --state=/data/tailscale/tailscaled.state --socket=/data/tailscale/tailscaled.sock --tun=tailscale0
 else
     echo "no need to start tailscaled"
 fi
